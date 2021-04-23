57f0bd339b86527d2aee31b865a1d161827bc16c
lvzh90 <linda.zapata@mulesoft.com>
2021-04-23 00:00:35 -0500

    feat(app): Create file and append data

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const writeFile = fs.createWriteStream('notes.txt');
 writeFile.write('Te chelo SS \n');
 writeFile.write('<3');
 writeFile.end();
+
+fs.appendFile('notes.txt', '\nFor ever', (err) => {
+    if (err) throw err;
+
+    console.log('The data was appended.')
+})
