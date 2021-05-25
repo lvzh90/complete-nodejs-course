74cbb95ef8c7af55a5e7e949a5b1c080de407765
lvzh90 <linda.zapata@mulesoft.com>
2021-05-25 11:15:22 -0500

    feat(app): Argument parsing with yargs

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-function getCommandsBuilder(command) {
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const { argv } = require('yargs');
 
-    const commandMatch = {
-        add: () => console.log('Adding note'),
-        remove: () => console.log('Removing note'),
-        edit: () => console.log('Editing note'),
-        default: () => console.log('Command no supported'),
-    }
+yargs(process.argv.slice(2))
+.command('add', 'Add a note', (yargs) => {
+    console.log('Adding note', yargs)
+})
+.argv
 
-    return (commandMatch[command] || commandMatch['default']);
-}
+yargs(hideBin(process.argv))
+.command('remove', 'Remove a note', (yargs) => {
+    console.log('Removing note')
+})
+.parse()
 
-const command = getCommandsBuilder(process.argv[2]);
-command();
+yargs(hideBin(process.argv))
+.command('edit', 'Editing a note', (yargs) => {
+    console.log('Editing note')
+})
+.parse()
+
+yargs(hideBin(process.argv))
+.command('list', 'List your notes', (yargs) => {
+    console.log('Listing out all note')
+})
+.parse()
+
+yargs(hideBin(process.argv))
+.command('read', 'Reading a note', (yargs) => {
+    console.log('Reading note')
+})
+.parse()
