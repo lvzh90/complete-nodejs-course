4d11de5e9a5277ae5451f941bf9e4cdb51b56382
Linda Vanessa Zapata Henao <linda.zapatah@perficient.com>
2021-09-23 17:17:13 -0500

    feat(app): Add advanced templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsDiectoryPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsDiectoryPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to server
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
@@ -24,7 +34,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        helpText: 'This is some helpful text.'
+        helpText: 'This is some helpful text.',
+        name: 'Linda Zapata'
     });
 })
 
