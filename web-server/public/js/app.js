3ee6381772a6bdccdfacff2e16bbccf6bc45822d
Linda Vanessa Zapata Henao <linda.zapatah@perficient.com>
2021-09-27 21:08:52 -0500

    feat(app): Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,15 @@
 console.log('Client side javascript file is loaded');
 
-fetch('http://localhost:3000/weather?address=boston').then(response => {
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const location = search.value;
+    const url = 'http://localhost:3000/weather?address=' + location;
+
+    fetch(url).then(response => {
     response.json().then((data) => {
         if (data.error) {
             console.log(data.error);
@@ -10,3 +19,6 @@ fetch('http://localhost:3000/weather?address=boston').then(response => {
         }
     })
 })
+
+})
+
