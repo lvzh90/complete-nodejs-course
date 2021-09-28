fed38495a778b21ea73d4e3af663db852d85c822
Linda Vanessa Zapata Henao <linda.zapatah@perficient.com>
2021-09-27 21:39:43 -0500

    feat(app): Wiring up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,6 +2,8 @@ console.log('Client side javascript file is loaded');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -9,13 +11,16 @@ weatherForm.addEventListener('submit', (event) => {
     const location = search.value;
     const url = 'http://localhost:3000/weather?address=' + location;
 
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
     fetch(url).then(response => {
     response.json().then((data) => {
         if (data.error) {
-            console.log(data.error);
+            messageOne.textContent = data.error;
         } else {
-            console.log(data.location);
-            console.log(data.forecast);
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast;
         }
     })
 })
