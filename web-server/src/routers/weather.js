09c722f98f5702bb368d9e82617c51e11bfb07aa
Linda Vanessa Zapata Henao <linda.zapatah@perficient.com>
2021-09-27 19:41:41 -0500

    feat(app): ES6 ASIDE - Default function parameters

--- web-server/src/routers/weather.js
+++ web-server/src/routers/weather.js
@@ -10,14 +10,14 @@ router.get('/weather', (req, res) => {
         })
     }
 
-    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
         if (error) {
             return res.send({ error })
         }
 
         forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
-                return console.log({ error });
+                return res.send({ error });
             }
 
             res.send({
